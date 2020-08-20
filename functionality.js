c5c09fad06bfa15080781dd065ef901ee55cd088
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-08-20 18:18:52 +0500

    calculator bodydone functionailty working on it

---
+++ functionality.js
@@ -0,0 +1,17 @@
+var number;
+var sign;
+
+function getNum(number) {
+  val_place = document.getElementById("result_box").value += number;
+}
+function op(sign) {
+  document.getElementById("result_box").value += sign;
+}
+function calc() {
+  result = document.getElementById("result_box").value;
+  document.getElementById("result_box").value = eval(result);
+}
+function reset() {
+  document.getElementById("result_box").value = "";
+}
+    
